1a8f002793c2148c461237f948ed0bd2e97822c3
Benjamin Richter <benjamin.richter@piraten-nom.de>
2012-12-09 00:59:31 +0100

    link calendar days to event list for that day

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,6 +4,9 @@ class CalendarController < ApplicationController
     @first_day_of_week = 1
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
+    if params[:day]
+      redirect_to :controller => "events", :action => "index", :year => @year, :month => @month, :day => params[:day]
+    end
 
     @shown_month = Date.civil(@year, @month)
 
